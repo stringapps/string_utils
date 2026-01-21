ddfba99ece40b93339f9a9ccc1f291959baa3199
d-code-hub <mailtodecode@gmail.com>
2026-01-21 19:03:28 +0530

    feat: Add Customer doctype JavaScript file and update doctype_js mapping

---
+++ string_utils/public/js/customer.js
@@ -0,0 +1,9 @@
+frappe.ui.form.on('Customer', {
+    refresh(frm) {
+    //    if (1===1) {
+    //     frappe.msgprint(__('Hiding Custom Prescription Tab'));
+    //         frm.set_df_property('custom_prescription', 'hidden', 1); // Hide the tab
+    //         // frm.refresh_field('custom_prescription');
+    //     }
+    },
+});
